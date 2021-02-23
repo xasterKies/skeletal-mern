15fe177346ab141ee3f1ee6c511886b868240888
TiokengSamuel <samueledward40@gmail.com>
2021-02-23 03:47:53 +0100

    config for production

--- webpack.config.server.js
+++ webpack.config.server.js
@@ -5,6 +5,22 @@ const nodeExternals = require('webpack-node-externals')
 
 const config = {
     name: "server",
+    entry: [path.join(CURRENT_WORKING_DIR, './server/server.js')],
+    target: "node",
+    output: {
+        path: path.join(CURRENT_WORKING_DIR, '/dist/'),
+        libraryTarget: "commonjs2"
+    },
+    externals: [nodeExternals()],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }
+        ]
+    }
 }
 
 module.exports = config
