8a6441668ca95e3329847fdf8408f3c96ed5f3a8
TiokengSamuel <samueledward40@gmail.com>
2021-02-23 04:18:45 +0100

    dist formating

--- server/server.js
+++ server/server.js
@@ -1,5 +1,11 @@
 import devBundle from './devBundle'
 import express from 'express'
+import path from 'path'
+
+const CURRENT_WORKING_DIR = process.cwd()
+
 devBundle.compile(app)
 
 const app = express()
+
+app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
