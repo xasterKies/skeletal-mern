d5f76428596541d75b075fc471ce58c03eca65ba
TiokengSamuel <samueledward40@gmail.com>
2021-02-23 03:14:48 +0100

    webpack server config

--- webpack.config.client.js
+++ webpack.config.client.js
@@ -3,7 +3,34 @@ const webpack = require('webpack')
 const CURRENT_WORKING_DIR = process.cwd();
 
 const config = {
-
+    name: "browser",
+    mode: "development",
+    devtool: 'eval-source-map',
+    entry: [
+        'webpack-hot-middle-ware/client?reload=true',path.join(CURRENT_WORKING_DIR, 'client/main.js')
+    ],
+    output: {
+        path: path.join(CURRENT_WORKING_DIR, '/dist'),
+        filename: 'bundle.js',
+        publicPath: '/dist/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }
+        ]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(), new webpack.NoEmitOnErrorsPlugin()
+    ],
+    resolve: {
+        alias: {
+            'react-dom': '@hot-loader/react-dom'
+        }
+    }
 }
 
 module.exports = config
--- webpack.config.server.js
+++ webpack.config.server.js
@@ -1,9 +1,10 @@
 const path = require('path');
 const webpack = require('webpack')
 const CURRENT_WORKING_DIR = process.cwd();
+const nodeExternals = require('webpack-node-externals')
 
 const config = {
-
+    name: "server",
 }
 
 module.exports = config
