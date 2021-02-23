26a9c94331e85e8eeb669c2b7c0da42eda418a7a
TiokengSamuel <samueledward40@gmail.com>
2021-02-23 04:33:24 +0100

    setting up server

--- server/server.js
+++ server/server.js
@@ -1,11 +1,26 @@
-import devBundle from './devBundle'
 import express from 'express'
+import devBundle from './devBundle'
 import path from 'path'
+import template from './../template'
+import mon
 
 const CURRENT_WORKING_DIR = process.cwd()
 
-devBundle.compile(app)
-
 const app = express()
 
+devBundle.compile(app)
+
 app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
+
+app.get('/', (req, res) => {
+    res.status(200).send(template())
+})
+
+let port = process.env.PORT || 3000
+
+app.listen(port, function onStart(err) {
+    if (err) {
+        console.log(err)
+    }
+    console.info('Server started on port %s. ', port)
+})
